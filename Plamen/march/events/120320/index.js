f8f89f82a8b3224b164c54af328e4aab606c352f
o0pmitev <pmitev89@gmail.com>
2020-03-12 13:32:47 +0100

    12-03-20

--- Plamen/march/events/120320/index.js
+++ Plamen/march/events/120320/index.js
@@ -1,6 +1,6 @@
 const newsletterBox = document.querySelector('#newsletter');
 
-const myFunction = () => {
+const hideFunction = () => {
   console.log('keydown');
   console.log('event');
 
@@ -9,4 +9,36 @@ const myFunction = () => {
   }
 }
 
-document.addEventListener('keydown', myFunction);
+
+const showFunction = () => {
+  console.log('keydown');
+  console.log('event');
+
+  if (event.keyCode === 13) {
+    newsletterBox.style.display = 'block';
+  }
+}
+
+document.body.addEventListener('keydown', hideFunction);
+document.body.addEventListener('keyup', showFunction);
+
+
+const mySubmit = () => {
+  // we need to stop the page of reloading after submit
+  event.preventDefault(); // this will remove the default behaviour of the event // this will stop the event to reload the page
+  console.log('Success' + ' ' + event.target.elements['email'].value);
+}
+
+const form = document.querySelector('form');
+form.addEventListener('submit', mySubmit);
+
+
+
+const myReset = () => {
+  event.preventDefault(); // this will remove the default behaviour of the event
+  console.log('Reset Success');
+}
+
+form.addEventListener('reset', myReset);
+
+
